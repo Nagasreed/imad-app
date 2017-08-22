29ad0db35b918454f893b71a7c694850879afda3
Nagasreed <30857629+Nagasreed@users.noreply.github.com>
2017-08-22 16:40:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,27 +6,27 @@ app.use(morgan('combined'));
 
 var articles={
     'articleone':{
-      // title:'Article-one|Nagasree D',
+       title:'Article-one|Nagasree D',
        heading:'Article-one',
        date:'Aug 22 2017',
        content:'<p>This is my first ArticleThis is my first Article ArticleThis is my first Article This is my first</p>'},
   'articletwo':{
-        //title:'Article-two|Nagasree D',
+        title:'Article-two|Nagasree D',
         heading:'Article-two',
         date:'Aug 28 2017',
         content:'<p>This is my Second Article</p>'},
 'articlethree': {
-       // title:'Article-three|Nagasree D',
+       title:'Article-three|Nagasree D',
         heading:'Article-three',
         date:'Aug 29 2017',
     content:'<p>This is my Third Article</p>'}
 };
-function createTemplate(data)
+function createTemplate(dat)
 {
-   // var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=data.content;
+   var title=dat.title;
+    var date=dat.date;
+    var heading=dat.heading;
+    var content=dat.content;
    var htmltemplate='<html><head> <meta name="viewpart" content="width-device-width" initial-scale=1/><link rel="stylesheet" type="text/css" href="/ui/style.css"></head><body><div class="container"> <div><a href="/">Home</a></div><h4>${heading}</h4> <div>${date}</div><div>${content}</div></body></html>';
 }
 app.get('/',function (req, res) {
