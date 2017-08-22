1aaa58107bdb5efb33d519958571ed3bdb555d0c
Nagasreed <30857629+Nagasreed@users.noreply.github.com>
2017-08-22 16:35:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
 
@@ -28,13 +27,11 @@ function createTemplate(data)
     var date=data.date;
     var heading=data.heading;
     var content=data.content;
-   var htmltemplate='<html><head><title>${title}</title> <meta name="viewpart" content="width-device-width" initial-scale=1/><link rel="stylesheet" type="text/css" href="/ui/style.css"></head><body><div class="container"> <div><a href="/">Home</a></div><h4>${heading}</h4> <div>${date}</div><div>${content}</div></body></html>';
+   var htmltemplate='<html><head> <meta name="viewpart" content="width-device-width" initial-scale=1/><link rel="stylesheet" type="text/css" href="/ui/style.css"></head><body><div class="container"> <div><a href="/">Home</a></div><h4>${heading}</h4> <div>${date}</div><div>${content}</div></body></html>';
 }
-
-app.get('/', function (req, res) {
+app.get('/',function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -47,7 +44,6 @@ app.get('/ui/madi.png', function (req, res) {
 });
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
-
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
