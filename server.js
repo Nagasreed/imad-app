cff657a6f1dfc3d9c1ef7855f1aa16a318fd8859
Nagasreed <30857629+Nagasreed@users.noreply.github.com>
2017-08-22 16:59:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -46,11 +46,12 @@ res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/:articleName', function (req, res){
+    var articleName = req.params.articleName;
 res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
-var articleName = req.params.articleName;
+
 res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
