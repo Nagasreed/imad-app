a335bc96b44b18660918118687143115b00de0d4
Nagasreed <30857629+Nagasreed@users.noreply.github.com>
2017-08-22 15:33:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,20 +6,20 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles={
-    article-one:{
+    articleone:{
         title:'Article-one|Nagasree D',
-        heading:'Article-one',
-        Date:'Aug 22 2017',
+        heading:"Article-one",
+        date:'Aug 22 2017',
         content:'<p>This is my first ArticleThis is my first Article This is my first ArticleThis is my first Article This is my first ArticleThis is my first Article This is my first ArticleThis is my first Article This is my first ArticleThis is my first Article This is my first ArticleThis is my first Article This is my first ArticleThis is my first ArticleThis is my first ArticleThiThis is my first ArticleThis is my first Article This is my first ArticleThis is my first Article</p>'
     },
-    article-two:
+    articletwo:
     {
         title:'Article-two|Nagasree D',
         heading:'Article-two',
         Date:'Aug 28 2017',
         content:'<p>This is my Second Article</p>'
     },
-    article-three:
+    articlethree:
     {
         title:'Article-three|Nagasree D',
         heading:'Article-three',
@@ -27,40 +27,16 @@ var articles={
         content:'<p>This is my Third Article</p>'
     }
 };
-var htmltemplate='
-<html>
-    <head>
-        <meta name="viewpart" content="width-device-width" initial-scale=1/>
-        <link rel="stylesheet" type="text/css" href="/ui/style.css">
-        </head>
-        <body>
-            <div class="container">
-                <div>
-                <a href="/">Home</a>
-                </div>
-            <hr>
-            <h4>Article one</h4>
-            <div>August 19 2017</div>
-           <div>
-            
-            <p>This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
-               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
-               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
-               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
-               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
-               This is my first Article</p>
-            <p>This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
-               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
-               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
-               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
-               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
-               This is my first Article</p>
-            </div>
-            </div>
-            </body>
-</html>';
+function createTemplate(data)
+{
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+   var htmltemplate='<html><head><title>${title}</title> <meta name="viewpart" content="width-device-width" initial-scale=1/><link rel="  stylesheet"   type="text/css" href="/ui/style.css"></head><body><div class="container"><div><a href="/">Home</a></div><h4>${heading}</h4> <div>${date}</div>  <div>${content}</div> </body></html>'
+  ;
 return htmltemplate;
-
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -70,19 +46,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article-one', function (req, res) {
-
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get(':/articleName', function (req, res) {
+var articleName=req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
-app.get('/article-two', function (req, res) {
-  
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three', function (req, res) {
-
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
