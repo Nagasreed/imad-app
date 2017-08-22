7561ad7e8949b1d51a5d931c11f514982c9da2fe
Nagasreed <30857629+Nagasreed@users.noreply.github.com>
2017-08-22 16:52:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,51 +1,105 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+
 var app = express();
 app.use(morgan('combined'));
 
-var articles={
-    'articleone':{
-       title:'Article-one|Nagasree D',
-       heading:'Article-one',
-       date:'Aug 22 2017',
-       content:'<p>This is my first ArticleThis is my first Article ArticleThis is my first Article This is my first</p>'},
-  'articletwo':{
-        title:'Article-two|Nagasree D',
-        heading:'Article-two',
-        date:'Aug 28 2017',
-        content:'<p>This is my Second Article</p>'},
-'articlethree': {
-       title:'Article-three|Nagasree D',
-        heading:'Article-three',
-        date:'Aug 29 2017',
-    content:'<p>This is my Third Article</p>'}
-};
-function createTemplate(data)
-{
-    var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=data.content;
-    var htmltemplate='<html><head> <meta name="viewpart" content="width-device-width" initial-scale=1/><link rel="stylesheet" type="text/css" href="/ui/style.css"></head><body><div class="container"> <div><a href="/">Home</a></div><h4>${heading}</h4> <div>${date}</div><div>${content}</div></body></html>';
-    return htmltemplate;
+var articles = {
+'article-one': {
+title: 'Article one .. Im Varun',
+heading: 'Article One',
+date: 'August 5,2017',
+content: 
+<p>
+This is the conent for my first article.This is the conent for my first article.This is the conent for my first article.This is the conent for my first article.This is the conent for my first article.This is the conent for my first article. 
+</p>
+<p>
+This is the conent for my first article.This is the conent for my first article.This is the conent for my first article.This is the conent for my first article.This is the conent for my first article.This is the conent for my first article. 
+</p>
+<p>
+This is the conent for my first article.This is the conent for my first article.This is the conent for my first article.This is the conent for my first article.This is the conent for my first article.This is the conent for my first article. 
+</p> 
+},
+'article-two': {
+title: 'Article Two .. Im Varun',
+heading: 'Article Two',
+date: 'August 10,2017',
+content: 
+<p>
+This is the conent for my second article. 
+</p>
+
+},
+'article-three': {
+  title: 'Article Three .. Im Varun',  
+  heading: 'Article Three',
+  date: 'August 23,2017',
+  content: `
+            <p>
+                     This is the conent for my third article.      
+            </p>`
+
 }
-app.get('/',function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+};
+
+function createTemplate (data) {
+var title = data.title;
+var date = data.date;
+var heading = data.heading;
+var content = data.content;
+var htmlTemplate = 
+<html>
+<head>
+<title>
+${title} 
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+<div class="container">
+<div>
+<a href="/">Home</a>
+</div>
+<hr/>
+<h3>
+${heading}
+</h3>
+<div>
+${date}
+</div>
+<div>
+${content}
+</div>
+</div>
+</body>
+</html>
+;
+return htmlTemplate;
+}
+
+app.get('/', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/:articleName', function (req, res){
+res.send(createTemplate(articles[articleName]));
+});
+
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+var articleName = req.params.arictleName;
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/:articleName', function (req, res) {
-var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
-});
+
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
+
 var port = 80;
 app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+console.log(IMAD course app listening on port ${port}!);
 });
