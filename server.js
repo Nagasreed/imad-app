d6a0b86607d6ce9a6060a38d9f2b77b40c6120a5
Nagasreed <30857629+Nagasreed@users.noreply.github.com>
2017-08-22 15:09:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,63 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+    article-one:{
+        title:'Article-one|Nagasree D',
+        heading:'Article-one',
+        Date:'Aug 22 2017',
+        content:'<p>This is my first ArticleThis is my first Article This is my first ArticleThis is my first Article This is my first ArticleThis is my first Article This is my first ArticleThis is my first Article This is my first ArticleThis is my first Article This is my first ArticleThis is my first Article This is my first ArticleThis is my first ArticleThis is my first ArticleThiThis is my first ArticleThis is my first Article This is my first ArticleThis is my first Article</p>'
+    },
+    article-two:
+    {
+        title:'Article-two|Nagasree D',
+        heading:'Article-two',
+        Date:'Aug 28 2017',
+        content:'<p>This is my Second Article</p>'
+    },
+    article-three:
+    {
+        title:'Article-three|Nagasree D',
+        heading:'Article-three',
+        Date:'Aug 29 2017',
+        content:'<p>This is my Third Article</p>'
+    }
+};
+var htmltemplate='
+<html>
+    <head>
+        <meta name="viewpart" content="width-device-width" initial-scale=1/>
+        <link rel="stylesheet" type="text/css" href="/ui/style.css">
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                <a href="/">Home</a>
+                </div>
+            <hr>
+            <h4>Article one</h4>
+            <div>August 19 2017</div>
+           <div>
+            
+            <p>This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
+               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
+               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
+               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
+               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
+               This is my first Article</p>
+            <p>This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
+               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
+               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
+               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
+               This is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first ArticleThis is my first Article
+               This is my first Article</p>
+            </div>
+            </div>
+            </body>
+</html>';
+return htmltemplate;
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
