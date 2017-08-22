0a59eb69febd820882904b6242eedcb1649762c2
Nagasreed <30857629+Nagasreed@users.noreply.github.com>
2017-08-22 16:43:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,11 +23,12 @@ var articles={
 };
 function createTemplate(data)
 {
-   var title=data.title;
+    var title=data.title;
     var date=data.date;
     var heading=data.heading;
     var content=data.content;
-   var htmltemplate='<html><head> <meta name="viewpart" content="width-device-width" initial-scale=1/><link rel="stylesheet" type="text/css" href="/ui/style.css"></head><body><div class="container"> <div><a href="/">Home</a></div><h4>${heading}</h4> <div>${date}</div><div>${content}</div></body></html>';
+    var htmltemplate='<html><head> <meta name="viewpart" content="width-device-width" initial-scale=1/><link rel="stylesheet" type="text/css" href="/ui/style.css"></head><body><div class="container"> <div><a href="/">Home</a></div><h4>${heading}</h4> <div>${date}</div><div>${content}</div></body></html>';
+    return htmltemplate;
 }
 app.get('/',function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
